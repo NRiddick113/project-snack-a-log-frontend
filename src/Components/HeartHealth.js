1e605890294ed17dd7c2eed99aeb225a130daeaf
JamalPartridge <jamalpartridge@pursuit.org>
2023-02-01 11:20:47 -0500

    hearthealt update

--- src/Components/HeartHealth.js
+++ src/Components/HeartHealth.js
@@ -1,10 +1,19 @@
 import heartSolid from "../assets/heart-solid.png";
 import heartOutline from "../assets/heart-regular.png";
 
+
 function HeartHealth({ snackHealth }) {
   return (
     <>
-      <p>????</p>
+      <p> {snackHealth.is_healthy ? (
+        <span>
+          <img src={heartSolid}></img>
+        </span>
+      ) : (
+        <span>
+          <img src={heartOutline}></img>
+        </span>
+      )}</p>
     </>
   );
 }
