a813f4a614e29aed0129f9119b5bc3e888853d6a
MarsiaRochelin <marsrochelin@pursuit.org>
2023-02-02 20:48:10 -0500

    tweak

--- src/Components/Snack.js
+++ src/Components/Snack.js
@@ -2,21 +2,20 @@ import { Link } from "react-router-dom";
 import heartSolid from "../assets/heart-solid.png";
 import heartOutline from "../assets/heart-regular.png";
 
-
 function Snack({ snack }) {
   return (
     <div>
-      <img src={snack.image}></img>
-       <p>
-        {snack.is_healthy ? 
-          <img src={heartSolid}></img>
-        : 
-          <img src={heartOutline}></img>
-        }
-      </p> 
       <Link to={`/snacks/${snack.id}`}>
-        <p>{snack.name}</p>
+        <img src={snack.image}></img>
       </Link>
+      <p>
+        {snack.is_healthy ? (
+          <img src={heartSolid}></img>
+        ) : (
+          <img src={heartOutline}></img>
+        )}
+      </p>
+      <p>{snack.name}</p>
     </div>
   );
 }
